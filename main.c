bfe537d06ebb879b0bfcbee4c30550d7fd64eab9
MANNYTECH01 <emmanueliseoluwa04@gmail.com>
2025-07-03 11:18:26 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+   srand(time(NULL));
+   int guess = 0;
+   int tries = 0;
+   int min = 10;
+   int max = 100;
+   int answer = (rand() % (max - min +1)) +min;
+
+   printf("***NUMBER GUESSING GAME ***");
+
+   do {
+    printf("Guess a number between %d - %d: ", min, max);
+    scanf("%d", &guess);
+    tries++;
+    if(guess < answer){
+        printf("TOO LOW\n");
+    }
+    else if(guess > answer){
+        printf("TOO HIGH\n");
+    }
+    else{
+        printf("CORRECT!\n");
+    }
+   }while(guess != answer);
+
+   printf("The answer is %d\n", answer);
+   printf("it took you %d tries", tries);
+   return 0;
+}
